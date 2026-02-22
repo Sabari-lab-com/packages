faed7323d25cb343f6d74d1187f6f402c92e9680
Sabari-lab-com <darktruths777@gmail.com>
2026-02-22 23:45:20 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-package-name",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="Simple greeting package",
+)
