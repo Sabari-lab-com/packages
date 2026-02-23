b8f718fc7f33095e91f0941aaa1bedb75582d399
Sabari-lab-com <darktruths777@gmail.com>
2026-02-23 12:16:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="Auto_train",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     install_requires=[],
     author="Sabari",
     description="this package is build and designed by srec stundent for final year project",
 
 )
+
 
 
