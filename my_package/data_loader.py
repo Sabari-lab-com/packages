01bacc9d045cfc8e72dfd8b55dfe6cb995a6e975
Sabari-lab-com <darktruths777@gmail.com>
2026-02-25 09:22:14 +0530

    Update data_loader.py

--- my_package/data_loader.py
+++ my_package/data_loader.py
@@ -13,11 +13,12 @@ def load_dataset():
     zip_path = list(uploaded.keys())[0]
 
     # Define target folder inside Drive
-    target_folder = "/content/drive/MyDrive/myfolder"
+    target_folder = "/content/drive/MyDrive/image_dataset"
     os.makedirs(target_folder, exist_ok=True)
 
     # Extract into Drive folder
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(target_folder)
 
+    print("Check your drive folder name:image_dataset \n copy the path your Dataset")
     return target_folder
