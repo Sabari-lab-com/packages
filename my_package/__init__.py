4d2fdb4b5bd3f79045c65905862f65c5100408a5
Sabari-lab-com <darktruths777@gmail.com>
2026-03-03 14:17:04 +0530

    Update __init__.py

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -1,5 +1,6 @@
 from .deeplearning_version3 import start_train
 from .moblie_net import start_train_mn
 from .efficient_net import start_train_efficient_net
+from .res_net import start_resnet
 from .data_loader import load_dataset
 from .model_export import save
